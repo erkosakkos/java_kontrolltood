fe6c4ffe9a0ec2a1070f75744b265b230fcf30e1
erkosakkos <erko.sakkos@eesti.ee>
2020-12-02 12:32:07 +0200

    Kontrolltöö nr.2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,54 @@
+import java.util.*;
+
 public class Main {
-
     public static void main(String[] args) {
-        int tableSize = 10;
-        int row = 0;
-        int column = 0;
+        burx chicken = new burx("kanaBurger", "kanaliha", "sai", 3.99);
+        burx cheese = new burx("juustuBurger", "loomaliha", "sai", 2.99);
+        burx taco = new burx("tacoBurger", "sealiha", 3.50);
 
-        for (row = 0; row < tableSize; row++) {
-            for (column = 0; column < tableSize; column++) {
-                if (row > column) {
-                    System.out.print(tableSize - 1 - row);
-                } else {
-                    System.out.print(tableSize - 1 - column);
+        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        double kokku = 0;
+
+        while(true) {
+            System.out.println("Vali burger: 1 - kanaBurger, 2 - juustuBurger, 3 - tacoBurger");
+            int valik = scanner.nextInt();
+            if(valik == 1) {
+                System.out.println(chicken);
+                chicken.lisaLisandid();
+                kokku += chicken.koostaBurger();
+                System.out.println("Burgeri hind: " + chicken.koostaBurger());
+                System.out.println("Kas soovid veel burgereid? (jah/ei)");
+                String veelBurgereid = reader.nextLine();
+                if(veelBurgereid.equals("ei")) {
+                    break;
+                }
+
+            }
+            else if(valik == 2) {
+                System.out.println(cheese);
+                cheese.lisaLisandid();
+                kokku += cheese.koostaBurger();
+                System.out.println("Burgeri hind: " + cheese.koostaBurger());
+                System.out.println("Kas soovid veel burgereid? (jah/ei)");
+                String veelBurgereid = reader.nextLine();
+                if(veelBurgereid.equals("ei")) {
+                    break;
                 }
             }
-            System.out.println();
+            else if(valik == 3) {
+                System.out.println(taco);
+                taco.lisaLisandid();
+                kokku += taco.koostaBurger();
+                System.out.println("Burgeri hind: " + taco.koostaBurger());
+                System.out.println("Kas soovid veel burgereid?  (jah/ei)");
+                String veelBurgereid = reader.nextLine();
+                if(veelBurgereid.equals("ei")) {
+                    break;
+                }
+            }
+            else break;
         }
+        System.out.println("Hind kokku on " + kokku + " €.");
     }
 }
